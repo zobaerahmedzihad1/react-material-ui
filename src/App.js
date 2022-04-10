8669d1a955fc4db114069b606685bb326b726ead
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-10 16:02:55 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Learn React Material ui</h2>
+    </div>
+  );
+}
+
+export default App;
